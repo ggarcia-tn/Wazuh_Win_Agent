abf0fb1999a4d3c8e9ffcadf1c892dfc360d14e3
Gabriel Garcia <ggarcia@trunorthcyber.com>
2026-01-19 00:11:46 -0500

    Updated with correct gh url

--- Powershell_VerCheck.ps1
+++ Powershell_VerCheck.ps1
@@ -1,6 +1,6 @@
 $dropLocation = "C:\temp\wazuh_agent"
-$Posh5ScriptUrl = "https://github.com/ggarcia-tn/Wazuh_Win_Agent/blob/59854d5883dee7e39b067a94c90d91dc4a4550c7/Agent_setupPS5.ps1"
-$Posh7ScriptUrl = "https://github.com/ggarcia-tn/Wazuh_Win_Agent/blob/59854d5883dee7e39b067a94c90d91dc4a4550c7/Agent_setupPS7.ps1"
+$Posh5ScriptUrl = "https://raw.githubusercontent.com/ggarcia-tn/Wazuh_Win_Agent/refs/heads/main/Agent_setupPS5.ps1"
+$Posh7ScriptUrl = "https://raw.githubusercontent.com/ggarcia-tn/Wazuh_Win_Agent/refs/heads/main/Agent_setupPS7.ps1"
 
 if($PSVersionTable.PSVersion.Major -eq 5){
 Curl -o $dropLocation\sysmon.zip -s -uri $Posh5ScriptUrl
